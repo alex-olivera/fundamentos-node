c61e48274733dd6aff67d32d591ff7f61c85c256
Alex Alexandrino <alex@heavyconnect.com>
2020-07-11 17:32:05 -0300

    Criando rotas de listagem de transações e criação de uma transação.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+const OUTCOME_VALUE = 'outcome';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is invalid.');
+    }
+
+    if (type === OUTCOME_VALUE) {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (total < value) {
+        throw Error('Insufficient balance.');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
